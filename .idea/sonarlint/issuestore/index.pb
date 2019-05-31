2ce366b046154b6ce1039643918ae64c1acf041d
Sam Vruggink <sam.vruggink@student.hu.nl>
2019-05-31 14:50:12 +0200

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+I
+springsecuritysigamis.iml,6/7/67dec5b7d75ef0968d44c2fef8bb95ac8967edb1
+P
+ src/main/java/mvc/MvcConfig.java,f/2/f2f31e17349eab22b95e78d5ac6993c3cef4a388
+`
+0src/main/java/websecurity/WebSecurityConfig.java,d/f/df2a5cdc039028e9a069b0171125591e1e737406
---
+++ src/main/java/mvc/MvcConfig.java
@@ -0,0 +1,18 @@
+package mvc;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/succes").setViewName("succes");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
